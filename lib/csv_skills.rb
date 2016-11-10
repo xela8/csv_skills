e71a607cbd4412ecde05356a98337a65a4d50f75
Alex <alex.v.karpov@gmail.com>
2016-11-09 17:35:12 -0800

    Write dummy results into the results file.

--- lib/csv_skills.rb
+++ lib/csv_skills.rb
@@ -6,8 +6,14 @@ require 'csv'
 def parse_file
   data = File.read("spec/fixtures/roster.csv")
 
-  write_file(domain_counts)
+  write_file
 end
 
-def write_file(domain_counts)
+def write_file
+  open('output.csv', 'w') { |f|
+    f  << "domain,count\n"
+    f  << "yahoo.com,331\n"
+    f  << "gmail.com,330\n"
+    f  << "hotmail.com,339\n"
+  }
 end
