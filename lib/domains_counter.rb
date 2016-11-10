d5dc99642cd15f959dabe15cc79f0f0c13e5ef34
Alex <alex.v.karpov@gmail.com>
2016-11-09 21:13:39 -0800

    Refactor domain and cell extraction into their own methods

--- lib/domains_counter.rb
+++ lib/domains_counter.rb
@@ -4,20 +4,27 @@ Bundler.require(:default)
 
 
 class DomainsCounter
-  def parse_file
-    domains_count = Hash.new(0)
-    File.open("spec/fixtures/roster.csv").each do |line|
-      domains_count[line.split(',')[1].split('@')[1]] += 1
+  def parse_csv(file, column_num)
+    domain_counts = Hash.new(0)
+    File.open(file).each do |line|
+      domain_counts[extract_domain(extract_cell(line, column_num))] += 1
     end
 
-    write_file(domains_count)
+    write_file(domain_counts)
   end
 
-  def write_file(domains_count)
+  def write_file(domain_counts)
     open('output.csv', 'w') { |f|
       f << "domain,count\n"
-      f << domains_count.map{|domain, count| "#{domain},#{count}"}.join("\n")
-
+      f << domain_counts.map{|domain, count| "#{domain},#{count}"}.join("\n")
     }
+  end
+
+  def extract_domain(email)
+    email.split('@').last
+  end
+
+  def extract_cell(line, col_num)
+    line.split(',')[col_num]
   end
 end
--- spec/domains_counter_spec.rb
+++ spec/domains_counter_spec.rb
@@ -7,7 +7,7 @@ describe DomainsCounter do
   let(:domains_counter) { DomainsCounter.new }
 
   it "generates a count of the domains used in email addresses" do
-    domains_counter.parse_file
+    domains_counter.parse_csv("spec/fixtures/roster.csv", 1)
 
     expect(CSV.read("output.csv")).to eq([
       ["domain", "count"],
@@ -15,5 +15,11 @@ describe DomainsCounter do
       ["gmail.com", "330"],
       ["hotmail.com", "339"],
     ])
+  end
+  it ".extact_domain extracts the domain from an email" do
+    expect(domains_counter.extract_domain('alex@gmail.com')).to eq('gmail.com')
+  end
+  it ".extract_cell from a csv row" do
+    expect(domains_counter.extract_cell('a,b,c', 1)).to eq('b')
   end
 end
